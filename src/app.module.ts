db73c0b939177824f713c27a728a551968873c23
Arnaud Mun <dev-id@macbook-pro-de-arnaud.local>
2025-10-16 11:13:40 +0200

    v1.3

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { ConfigModule, ConfigService } from '@nestjs/config';
@@ -23,6 +22,5 @@ import { NodemailerModule } from './nodemailer/nodemailer.module';
     NodemailerModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
